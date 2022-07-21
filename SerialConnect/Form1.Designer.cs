eb820d03f3355205efe544674ccd9c7f24888e0f
adnKsharp <darkmark404@protonmail.com>
2022-07-21 12:28:55 -0600

    First...

---
+++ SerialConnect/Form1.Designer.cs
@@ -0,0 +1,207 @@
+﻿namespace SerialConnect
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variable del diseñador necesaria.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpiar los recursos que se estén usando.
+		/// </summary>
+		/// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código generado por el Diseñador de Windows Forms
+
+		/// <summary>
+		/// Método necesario para admitir el Diseñador. No se puede modificar
+		/// el contenido de este método con el editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			this.label1 = new System.Windows.Forms.Label();
+			this.comboBox1 = new System.Windows.Forms.ComboBox();
+			this.comboBox2 = new System.Windows.Forms.ComboBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button1 = new System.Windows.Forms.Button();
+			this.label4 = new System.Windows.Forms.Label();
+			this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+			this.button2 = new System.Windows.Forms.Button();
+			this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+			this.tableLayoutPanel1.SuspendLayout();
+			this.tableLayoutPanel2.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(14, 9);
+			this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 16);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "label1";
+			// 
+			// comboBox1
+			// 
+			this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox1.Font = new System.Drawing.Font("Hack NF", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.comboBox1.FormattingEnabled = true;
+			this.comboBox1.Location = new System.Drawing.Point(163, 3);
+			this.comboBox1.Name = "comboBox1";
+			this.comboBox1.Size = new System.Drawing.Size(106, 23);
+			this.comboBox1.TabIndex = 1;
+			// 
+			// comboBox2
+			// 
+			this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBox2.Font = new System.Drawing.Font("Hack NF", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.comboBox2.FormattingEnabled = true;
+			this.comboBox2.Location = new System.Drawing.Point(163, 35);
+			this.comboBox2.Name = "comboBox2";
+			this.comboBox2.Size = new System.Drawing.Size(106, 23);
+			this.comboBox2.TabIndex = 4;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(3, 32);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(43, 16);
+			this.label3.TabIndex = 3;
+			this.label3.Text = "label3";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(3, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(43, 16);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "label2";
+			// 
+			// button1
+			// 
+			this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(34)))), ((int)(((byte)(42)))));
+			this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button1.FlatAppearance.BorderSize = 0;
+			this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+			this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(44)))), ((int)(((byte)(53)))));
+			this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button1.Location = new System.Drawing.Point(179, 99);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(105, 23);
+			this.button1.TabIndex = 3;
+			this.button1.Text = "Desconectar";
+			this.button1.UseVisualStyleBackColor = false;
+			this.button1.Click += new System.EventHandler(this.B1C);
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(252)))), ((int)(((byte)(201)))), ((int)(((byte)(32)))));
+			this.label4.Location = new System.Drawing.Point(3, 0);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(43, 16);
+			this.label4.TabIndex = 4;
+			this.label4.Text = "label4";
+			// 
+			// button2
+			// 
+			this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(29)))), ((int)(((byte)(34)))), ((int)(((byte)(42)))));
+			this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
+			this.button2.FlatAppearance.BorderSize = 0;
+			this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Black;
+			this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(44)))), ((int)(((byte)(53)))));
+			this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+			this.button2.Location = new System.Drawing.Point(12, 99);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(161, 23);
+			this.button2.TabIndex = 5;
+			this.button2.Text = "Buscar puertos";
+			this.button2.UseVisualStyleBackColor = false;
+			this.button2.Click += new System.EventHandler(this.B2C);
+			// 
+			// tableLayoutPanel1
+			// 
+			this.tableLayoutPanel1.ColumnCount = 2;
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 58.82353F));
+			this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 41.17647F));
+			this.tableLayoutPanel1.Controls.Add(this.label2, 0, 0);
+			this.tableLayoutPanel1.Controls.Add(this.comboBox2, 1, 1);
+			this.tableLayoutPanel1.Controls.Add(this.label3, 0, 1);
+			this.tableLayoutPanel1.Controls.Add(this.comboBox1, 1, 0);
+			this.tableLayoutPanel1.Location = new System.Drawing.Point(12, 28);
+			this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+			this.tableLayoutPanel1.RowCount = 2;
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel1.Size = new System.Drawing.Size(272, 65);
+			this.tableLayoutPanel1.TabIndex = 5;
+			// 
+			// tableLayoutPanel2
+			// 
+			this.tableLayoutPanel2.ColumnCount = 1;
+			this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel2.Controls.Add(this.label4, 0, 0);
+			this.tableLayoutPanel2.Location = new System.Drawing.Point(12, 128);
+			this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+			this.tableLayoutPanel2.RowCount = 1;
+			this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+			this.tableLayoutPanel2.Size = new System.Drawing.Size(270, 60);
+			this.tableLayoutPanel2.TabIndex = 6;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(39)))), ((int)(((byte)(47)))));
+			this.ClientSize = new System.Drawing.Size(294, 200);
+			this.Controls.Add(this.tableLayoutPanel2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.tableLayoutPanel1);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.label1);
+			this.Font = new System.Drawing.Font("SF Pro Text", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.ForeColor = System.Drawing.Color.White;
+			this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+			this.Name = "Form1";
+			this.Text = "Puerto Serie";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.tableLayoutPanel1.ResumeLayout(false);
+			this.tableLayoutPanel1.PerformLayout();
+			this.tableLayoutPanel2.ResumeLayout(false);
+			this.tableLayoutPanel2.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.ComboBox comboBox1;
+		private System.Windows.Forms.ComboBox comboBox2;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label4;
+		private System.IO.Ports.SerialPort serialPort1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+		private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+	}
+}
+
---
+++ SerialConnect/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SerialConnect
+{
+	public partial class Form1 : Form
+	{
+		bool connect = false;
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			label1.Text = "Conexión por puerto serie";
+			button1.Text = "Conectar";
+			button2.Text = "Buscar puertos";
+			label2.Text = "Velocidad de conexión:";
+			label3.Text = "Puerto:";
+			label4.Text = "";
+			SetComboBox();
+		}
+
+		private void B1C(object sender, EventArgs e)
+		{
+			connect = !connect;
+			if (connect && !serialPort1.IsOpen)
+			{
+				if (comboBox2.SelectedItem != null)
+				{
+					serialPort1.PortName = comboBox2.SelectedItem.ToString();
+					serialPort1.BaudRate = int.Parse(comboBox1.SelectedItem.ToString());
+					try
+					{
+						serialPort1.Open();
+						button1.Text = "Desconectar";
+						label4.Text = "Conexión con el puerto " + comboBox2.SelectedItem.ToString() + " exitosa";
+					}
+					catch
+					{
+						label4.Text = "Error al conectarse al puerto " + comboBox2.SelectedItem.ToString();
+					}
+				}
+				else
+					label4.Text = "Puerto serie no valido";
+			}
+			else if (serialPort1.IsOpen)
+			{
+				button1.Text = "Conectar";
+				serialPort1.Close();
+				label4.Text = "Conexión finalizada";
+			}
+		}
+
+		private void B2C(object sender, EventArgs e)
+		{
+			SetComboBox();
+		}
+
+		public void SetComboBox()
+		{
+			string[] ports = System.IO.Ports.SerialPort.GetPortNames();
+			comboBox1.Items.Clear();
+			comboBox2.Items.Clear();
+
+			for (int i = 1; i < Math.Pow(2, 6); i *= 2)
+				comboBox1.Items.Add(300 * i);
+			comboBox1.Items.Add(14400);
+			comboBox1.Items.Add(19200);
+			for (byte i = 1; i < 4; i++)
+				comboBox1.Items.Add((i * 9600) + 19200);
+			comboBox1.Items.Add(57600);
+			comboBox1.Items.Add(115200);
+			foreach (string port in ports)
+				comboBox2.Items.Add(port);
+
+			comboBox1.SelectedIndex = 5;
+			if (comboBox2.Items.Count > 0)
+				comboBox2.SelectedIndex = 0;
+		}
+	}
+}
